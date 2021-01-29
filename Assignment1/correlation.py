da3e10e5a95677940b64f3de054186c5a22dd8fa
Timothy <timothymin@gatech.edu>
2021-01-28 23:25:09 -0800

    changed

--- Assignment1/correlation.py
+++ Assignment1/correlation.py
@@ -1,20 +1,22 @@
 # Digital-Signal-Processing
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.io import wavfile
 
 def readfile(filename):
+
     samplerate, data = wavfile.read(filename)
     return data[:,0]
 def crossCorr(x,y):
-    lengthx = len(x)
-    lengthy = len(y)
+    z = np.correlate(x,y)
+    return z
 
 
 if __name__ == '__main__':
 
-    x=readfile('./Assignment01/drum_loop.wav')
+    x = readfile('drum_loop.wav')
     lengthx = len(x)
-    y = readfile('./Assignment01/snare.wav')
+    y = readfile('snare.wav')
     lengthy = len(y)
     z = crossCorr(x,y)
     plt.plot(z)
