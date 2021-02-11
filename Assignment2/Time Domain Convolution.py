6a352a811014119dd2f30a2148057a13b3cc69bc
Timothy <timothymin@gatech.edu>
2021-02-10 19:15:46 -0800

    changed but still need to bug

--- Assignment2/Time Domain Convolution.py
+++ Assignment2/Time Domain Convolution.py
@@ -1,37 +1,39 @@
-
 # Digital-Signal-Processing
 
 import numpy as np
 import matplotlib
 
-def myTimeConv(x,h):
-# make simple rectangular signals
 
-    x1 = np.ones(x)  #signal
-    h1 = np.ones(h)    #impulse response
-    print (x1)
-    print (h1)
-    fx = np.fft.rfft(x1)           # fftransform the signals
+def ConmpareConv(x, h):
+
+
+def myTimeConv(x, h):
+    # make simple rectangular signals
+
+    x1 = np.ones(x)  # signal
+    h1 = np.ones(h)  # impulse response
+    print(x1)
+    print(h1)
+    fx = np.fft.rfft(x1)  # fftransform the signals
     fh = np.fft.rfft(h1)
-   # ffx = np.squeeze(np.asarray(fx))
-    #ffh = np.squeeze(np.asarray(fh))
-   # ffx = fx.reshape(1, -1) # X.shape == (1, 2500)
-  #  ffh = fh.reshape(1, -1) # Y.shape == (1, 2)
-    fxh = np.dot(fx,fh)  # multiply the frequency spectrum of the signals
+    # ffx = np.squeeze(np.asarray(fx))
+    # ffh = np.squeeze(np.asarray(fh))
+    # ffx = fx.reshape(1, -1) # X.shape == (1, 2500)
+    #  ffh = fh.reshape(1, -1) # Y.shape == (1, 2)
+    fxh = np.dot(fx, fh)  # multiply the frequency spectrum of the signals
 
-    y1 = np.fft.irfft(fxh) # ifftransform back to time-domain
-    y = np.ones(y1)
-    return y
+    y_time = np.fft.irfft(fxh)  # ifftransform back to time-domain
+
+    return y_time
 
 
 if __name__ == '__main__':
+    t1 = np.linspace(0, 1, num=25)
+    t2 = np.linspace(1, 0, num=26)
+    h = np.append(t1, t2)
+    print(h)
 
-    x = (200,1)
-    h = (100,1)
-    # x = readfile('drum_loop.wav')
-    # lengthx = len(x)
-    # y = readfile('snare.wav')
-    # lengthy = len(y)
-    Convolution = myTimeConv(x,h)
-    print (Convolution)
+    x = (200, 1)
+    Convolution = myTimeConv(x, h)
+    print(Convolution)
 
