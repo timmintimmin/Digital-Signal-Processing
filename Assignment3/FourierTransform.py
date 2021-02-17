bd57dd55df79e07ebeda21032b617d271afa4da7
Timothy <timothymin@gatech.edu>
2021-02-17 00:43:37 -0800

    lol rip part 2 but im proud of what you have rn

--- Assignment3/FourierTransform.py
+++ Assignment3/FourierTransform.py
@@ -43,16 +43,43 @@ def generateSquare(a,b,c,d,e):
     CT = 1 / sampling_rate_Hz
     signal = (2 * np.pi * frequency_Hz)
     t = np.arange(0, d, CT)
-    x2= np.sign(np.sin(2*(np.pi)*t*(frequency_Hz)))
+    sinusoidals = (1,3,5,7,9,11,13,15,17,19)
+    sumsin = 0
+    for number in sinusoidals:
+        sumsin = ((1/number)*np.sin(number*2*np.pi*frequency_Hz*t))+ sumsin
+    print ("this is sumsin")
+    print (sumsin)
+
+
+    x2 = (4/(np.pi)*(sumsin))
+
+
+
+    #x2= np.sign(np.sin(2*(np.pi)*t*(frequency_Hz)))
     #x2= np.sign(np.cos(2*(np.pi)*t*(frequency_Hz)))
     
     #signal = np.int16(signal)
     # sine = (amplitude)* np.sin((frequency_Hz)*(time))
 
 
-    x = (amplitude) * np.sin((x2 * t) + phase_radians)
+    #x = (amplitude) * np.sin((x2 * t) + phase_radians)
     
     return (t,x2)
+
+def computeSpectrum(x, y):
+    sample_rate_Hz = y
+    st = 1/(y)   #sampling time
+    spectrum = np.fft.fft(x)
+
+
+    XAbs = np.abs(spectrum)#magnitude spectrum/amplitude
+    print (XAbs)
+    XPhase = np.angle(spectrum) #phase spectrum
+    #XRe = #real part
+    #XIm = #imaginary
+    f = np.fft.fftfreq(x)#frequency of bins
+    print (f)
+
 
 
 if __name__ == '__main__':
@@ -69,6 +96,7 @@ if __name__ == '__main__':
     plt.show()
     t,x2 = generateSquare(1.0, 44100, 400, 0.5, 0)
     print(t)
+    print ("this is x2")
     print(x2)
     plt.plot(t, x2)
     plt.xlim(0,0.005)
@@ -76,3 +104,5 @@ if __name__ == '__main__':
     plt.ylabel("Generated Signal")
     plt.xlabel("Time")
     plt.show()
+    a,b,c,d,e = computeSpectrum((x),44100)
+    a2,b2,c2,d2,e2 = computeSpectrum((x2),44100)
