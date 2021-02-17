6f46f0f79ca97ca36183aec1064abc6773a8f11f
timmintimmin <78189299+timmintimmin@users.noreply.github.com>
2021-02-16 21:22:49 -0800

    Update FourierTransform.py

--- Assignment3/FourierTransform.py
+++ Assignment3/FourierTransform.py
@@ -13,12 +13,17 @@ def generateSinusoidal(a,b,c,d,e):
     frequency_Hz = (c)
     length_secs = (d)
     phase_radians = (e)
-    #time = np.arange(0,length_secs, 1)
+    
+ 
+    #t = np.linspace(0,(length_secs),np.multiply((length_secs)*(sampling_rate_Hz))+1)
+    #x = np.sin(2*np.pi*(frequency_Hz)*t)
+    
+    time = np.arange(0,length_secs, 1)
     samples = (length_secs)*(sampling_rate_Hz)
     CT = 1 / sampling_rate_Hz
     signal = (2 * np.pi * frequency_Hz)
     #signal = np.int16(signal)
-    #sine = (amplitude)* np.sin((frequency_Hz)*(time))
+    sine = (amplitude)* np.sin((frequency_Hz)*(time))
     t = np.arange(0,d, CT)
 
     x = (amplitude)* np.sin((signal * t) + phase_radians)
@@ -39,14 +44,15 @@ def generateSquare(a,b,c,d,e):
     signal = (2 * np.pi * frequency_Hz)
     t = np.arange(0, d, CT)
     x2= np.sign(np.sin(2*(np.pi)*t*(frequency_Hz)))
-    #v(t)= np.sign(np.cos(2*(np.pi)*t*(frequency_Hz)))
-    # signal = np.int16(signal)
+    #x2= np.sign(np.cos(2*(np.pi)*t*(frequency_Hz)))
+    
+    #signal = np.int16(signal)
     # sine = (amplitude)* np.sin((frequency_Hz)*(time))
 
 
     x = (amplitude) * np.sin((x2 * t) + phase_radians)
     
-    return (t,x)
+    return (t,x2)
 
 
 if __name__ == '__main__':
@@ -54,15 +60,18 @@ if __name__ == '__main__':
     t,x = generateSinusoidal(1.0, 44100, 400, 0.5, np.pi/2)
     print (t)
     print (x)
-    plt.plot(t,x)
+    
+    plt.plot(t,(x))
+    plt.xlim(0,0.005)
     plt.title("Question 1")
     plt.ylabel("Generated Signal")
     plt.xlabel("Time")
     plt.show()
-    t,x = generateSquare(1.0, 44100, 400, 0.5, np.pi/2)
+    t,x2 = generateSquare(1.0, 44100, 400, 0.5, 0)
     print(t)
-    print(x)
-    plt.plot(t, x)
+    print(x2)
+    plt.plot(t, x2)
+    plt.xlim(0,0.005)
     plt.title("Question 2")
     plt.ylabel("Generated Signal")
     plt.xlabel("Time")
