d22196f7d31bc16e29b771e111b0debaa424377e
Timothy <timothymin@gatech.edu>
2021-05-04 14:40:55 -0700

    revised the LPF

--- Final for DSP.py
+++ Final for DSP.py
@@ -96,80 +96,15 @@ def tonal_correction_filter(input_signal, alpha):
 
 def HighPassFilter():
     sr, x = wavfile.read('Water-Dripping-A4-www.fesliyanstudios.com.wav')  # 16-bit mono 44.1 khz
-    b = scipy.signal.firwin(101, cutoff=1000, fs=sr, pass_zero=False)     #the number after cutoff determines cutoff freq
+    b = scipy.signal.firwin(101, cutoff=1000, fs=sr, pass_zero=False)     #the number after cutoff determines cutoff freq #changeable
     x = scipy.signal.lfilter(b, [1.0], x)
     wavfile.write('HPFed.wav', sr, x.astype(np.int16))
 
 def LowPassFilter():
-    stream = wave.open('Water-Dripping-A4-www.fesliyanstudios.com.wav', "rb")
-
-    n_channels = stream.getnchannels()
-    sample_rate = stream.getframerate()
-    sample_width = stream.getsampwidth()
-    n_frames = stream.getnframes()
-
-    raw_data = stream.readframes(n_frames)  # Returns byte data
-    stream.close()
-
-    interpret_wav(raw_data, n_frames, n_channels, sample_width)
-    wav_file.close()
-
-def running_mean(x, windowSize):
-    cumsum = np.cumsum(np.insert(x, 0, 0))
-    return (cumsum[windowSize:] - cumsum[:-windowSize]) / windowSize
-
-
-# from http://stackoverflow.com/questions/2226853/interpreting-wav-data/2227174#2227174
-def interpret_wav(raw_bytes, n_frames, n_channels, sample_width, interleaved=True):
-
-
-
-    if sample_width == 1:
-        dtype = np.uint8  # unsigned char
-    elif sample_width == 2:
-        dtype = np.int16  # signed 2-byte short
-    else:
-        raise ValueError("Only supports 8 and 16 bit audio formats.")
-
-    channels = np.frombuffer(raw_bytes, dtype=dtype)
-
-    if interleaved:
-        # channels are interleaved, i.e. sample N of channel M follows sample N of channel M-1 in raw data
-        channels.shape = (n_frames, n_channels)
-        channels = channels.T
-    else:
-        # channels are not interleaved. All samples from channel M occur before all samples from channel M-1
-        channels.shape = (n_channels, n_frames)
-
-    return channels
-
-
-with contextlib.closing(wave.open('Water-Dripping-A4-www.fesliyanstudios.com.wav', 'rb')) as spf:
-
-        sampleRate = spf.getframerate()
-        ampWidth = spf.getsampwidth()
-        nChannels = spf.getnchannels()
-        nFrames = spf.getnframes()
-        fname = 'Water-Dripping-A4-www.fesliyanstudios.com.wav'
-        outname = 'LPFed.wav'
-
-    # Extract Raw Audio from multi-channel Wav File
-        signal = spf.readframes(nFrames * nChannels)
-        spf.close()
-        channels = interpret_wav(signal, nFrames, nChannels, ampWidth, True)
-        cutOffFrequency = 400.0 # Change this parameter to change cut off frequency!
-    # get window size
-    # from http://dsp.stackexchange.com/questions/9966/what-is-the-cut-off-frequency-of-a-moving-average-filter
-        freqRatio = (cutOffFrequency / sampleRate)
-        N = int(math.sqrt(0.196196 + freqRatio ** 2) / freqRatio)
-
-    # Use moving average (only on first channel)
-        filtered = running_mean(channels[0], N).astype(channels.dtype)
-
-        wav_file = wave.open(outname, "w")
-        wav_file.setparams((1, ampWidth, sampleRate, nFrames, spf.getcomptype(), spf.getcompname()))
-        wav_file.writeframes(filtered.tobytes('C'))
-        wav_file.close()
+    sr, x = wavfile.read('Water-Dripping-A4-www.fesliyanstudios.com.wav')  # 16-bit mono 44.1 khz
+    b = scipy.signal.firwin(101, .9)  # the 2nd number determines the low pass freq (choose between 0 and 1) # Changeable
+    x = scipy.signal.lfilter(b, [1.0], x)
+    wavfile.write('LPFed.wav', sr, x.astype(np.int16))
 
 
 def Flanger():
@@ -179,12 +114,12 @@ def Flanger():
 
     output = 'Flanged.wav'
 
-    fmod = 0.1
+    fmod = 0.3      #changeable
     A = int(0.005 * sr)
     M = int(0.005 * sr)
     BL = 0.7
     FF = 0.7
-    FB = -0.7
+    FB = -0.7      #changeable
 
     if A > M:
         raise RuntimeError("Amplitude of vibrato too high for delay length")
@@ -219,7 +154,7 @@ def Chorus():
 
     output = 'Chorused.wav'
 
-    fmod = 1.5
+    fmod = 1.2      #changeable
     A = int(0.002 * sr)
     M = int(0.002 * sr)
     BL = 1.0
